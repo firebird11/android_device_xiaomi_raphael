3f2f03ac09f1e6fefdc643a8eb402b3fa45ce320
firebird11 <hbgassel@gmail.com>
2023-11-25 05:27:53 +0100

    raphael: Add Miuicamera
    Change-Id: I30b938db5aed53ea45c33e740ef2ef8a666f7dfe

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/raphael/raphael-vendor.mk)
 
+# MiuiCamera
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+
 # Get Qcom components
 TARGET_EXCLUDE_QCOM_SEPOLICY := true
 TARGET_ADRENO_COMPONENT_VARIANT := adreno-r
@@ -133,15 +136,16 @@ TARGET_ENABLE_BLUR := true
 
 # Camera
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.motor@1.0.vendor \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     libdng_sdk.vendor \
+    libgui_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
-    libgui_vendor \
-    libstdc++_vendor
+    vendor.xiaomi.hardware.motor@1.0.vendor
+
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
